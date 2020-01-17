1252313c82a6fe2a8eb2a3434b20ee05f793aa44
rkaushik29 <rohit.rk.rk1@gmail.com>
2020-01-16 21:46:06 -0500

    shooting and player movements

---
+++ game/Assets/Scripts/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Vector2 bulletDirection;
+    public float bulletSpeed;
+    // Start is called before the first frame update
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+        
+    }
+
+    private void Shoot() {
+        GameObject bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
